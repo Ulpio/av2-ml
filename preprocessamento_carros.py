7a3b4bf87121913153270bf1510635b631616e31
Ulpio <ulpionetto0@gmail.com>
2025-06-03 20:44:02 -0300

    pre processamento

--- preprocessamento_carros.py
+++ preprocessamento_carros.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
 
 # Carregar os dados
 df = pd.read_excel("p40.xlsx", sheet_name="2005 CAR DATA")
@@ -26,6 +28,11 @@ y = df_encoded["Leather"]
 
 # Separar dados em treino e teste
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 # Salvar os conjuntos processados em CSVs (opcional)
 X_train.to_csv("X_train.csv", index=False)
